c3bfe44cbbbd8eb979bdbc5b82b964a3ede4b435
rnunesil <renan.nuness95@gmail.com>
2019-08-21 13:56:22 -0300

    Initial commit

---
+++ app/src/main/java/br/com/renannunessil/navigationcomponentsample/BlankFragment.kt
@@ -0,0 +1,30 @@
+package br.com.renannunessil.navigationcomponentsample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_blank.*
+
+class BlankFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_blank, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bt_next.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_blankFragment_to_blankFragment2)
+        }
+    }
+}
